d88b10e8a8b6afdc70e870b1855e2a17cb255001
Imer Lopez <2018119251@ub.edu.bz>
2022-09-08 16:13:11 -0600

    rename table to entries

--- migrations/000001_kriol.up.sql
+++ migrations/000001_kriol.up.sql
@@ -1,7 +1,8 @@
 --Filename: 000001_kriol.up.sql
 
 
-CREATE TABLE IF NOT EXISTS kriol(
+CREATE TABLE IF NOT EXISTS entries(
+
     english_word VARCHAR ( 100 ) UNIQUE NOT NULL,
     kriol_word VARCHAR ( 100 ) UNIQUE NOT NULL
 );
