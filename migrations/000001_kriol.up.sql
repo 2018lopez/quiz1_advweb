4515a26e1bdacabbe28ad568140b085e21a675cd
Imer Lopez <2018119251@ub.edu.bz>
2022-09-08 16:47:38 -0600

    added Primary key to entries table

--- migrations/000001_kriol.up.sql
+++ migrations/000001_kriol.up.sql
@@ -3,6 +3,7 @@
 
 CREATE TABLE IF NOT EXISTS entries(
 
+    id bigserial PRIMARY KEY,
     english_word VARCHAR ( 100 ) UNIQUE NOT NULL,
     kriol_word VARCHAR ( 100 ) UNIQUE NOT NULL
 );
---
+++ migrations/000002_alter_kriol.up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE entries ADD COLUMN id bigserial PRIMARY KEY;
